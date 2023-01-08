6637d499f6f7542d686aaeb007ace9741f2ce735
panmourovaty <35844848+panmourovaty@users.noreply.github.com>
2023-01-08 02:58:54 +0100

    add cron to run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 cd /var/www/app
 sudo -u nginx php admin/cli/upgrade.php --allow-unstable --non-interactive || true
+cron
 nginx -g 'daemon off;' &
 P1=$!
 php-fpm8.0 &
-P2=$!
+P2=$! &
 wait $P1 $P2
